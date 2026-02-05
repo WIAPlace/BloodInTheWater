1fa8422ebd684e6394365c8ed0bc0d2f1f4cfb4e
WesInAPlace <143664522+WIAPlace@users.noreply.github.com>
2026-02-05 16:18:37 -0600

    Added Input Reader script

--- Assets/Code/Input/InputReader.cs
+++ Assets/Code/Input/InputReader.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// 
 /// Author: Weston Tollette
@@ -13,7 +15,74 @@ using UnityEngine;
 /// Edit Purpose:
 /// 
 
-public class InputReader : ScriptableObject
+public class InputReader : ScriptableObject, GameInput.IGamePlayActions, GameInput.IUIActions
 {
-    
+    private GameInput gameInput;
+
+    void OnEnable()
+    { // Enable this When the game begins.
+        if (gameInput == null)
+        {
+            gameInput=new GameInput();
+            gameInput.GamePlay.SetCallbacks(this);
+            gameInput.UI.SetCallbacks(this);
+        }
+    }
+    void OnDisable()
+    { // Disable this when the game Stops.
+        if(gameInput != null)
+        {
+           gameInput.Disable(); 
+        }
+    }
+
+    public void SetGameplay()
+    {
+        gameInput.GamePlay.Enable();
+        gameInput.UI.Disable();
+    }
+    public void SetUI()
+    {
+        gameInput.GamePlay.Disable();
+        gameInput.UI.Enable();
+    }
+
+    public event Action<Vector2> MoveEvent;
+    public event Action InteractEvent;
+
+    public event Action PauseEvent;
+    public event Action ResumeEvent;
+
+
+
+    public void OnInteract(InputAction.CallbackContext context)
+    { // Interact
+        if (context.phase == InputActionPhase.Performed)
+        {
+            InteractEvent?.Invoke();
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    { // Move (Sends a vector 2 beacuse the values considered are the cardinal directions.)
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    { // Pause 
+        if (context.phase == InputActionPhase.Performed)
+        {
+            PauseEvent?.Invoke();
+            SetUI(); // Make it so that only UI controls are able to be done.
+        }
+    }
+
+    public void OnResume(InputAction.CallbackContext context)
+    { // Resume
+        if (context.phase == InputActionPhase.Performed)
+        {
+            ResumeEvent?.Invoke();
+            SetGameplay(); // Make it so that only gameplay controls are able to be done.
+        }
+    }
 }
