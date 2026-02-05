ad64676cc3ee3247683de3ca55cdcdbbb2063435
WesInAPlace <143664522+WIAPlace@users.noreply.github.com>
2026-02-05 15:33:11 -0600

    Begun Input Menu Stuff

---
+++ Assets/Code/Input/InputReader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 
+/// Author: Weston Tollette
+/// Created: 2/5/26
+/// Purpose: This is a tool to communicate between the GameInput Script and the scripts that call for those inputs.
+///     This will allow us to not have to reform the connections from inputsystem and script uses in each and every script used. 
+/// 
+/// Edited:
+/// Edited By:
+/// Edit Purpose:
+/// 
+
+public class InputReader : ScriptableObject
+{
+    
+}
