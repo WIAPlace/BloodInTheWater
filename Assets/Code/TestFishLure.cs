d72d0ee9c924428e6bb04d5bbacaae011c3c2291
GobAL-0 <aden.lopez4689@gmail.com>
2026-02-08 21:11:58 -0600

    Revert "Merge branch 'main' of https://github.com/WIAPlace/BloodInTheWater"
    This reverts commit e3eb93fd1f5b7e8e321aed550e823580ade77583, reversing
    changes made to a4d1faef2188701b7212c5b3d523625415a8e133.

--- Assets/Code/TestFishLure.cs
+++ Assets/Code/TestFishLure.cs
@@ -6,10 +6,9 @@ using UnityEngine;
 ///     Definitly not final, mainly this will just spawn and despawn it 
 ///     This should be attached to the camera for now
 /// 
-/// Edited: 2/8/26
-/// Edited By: Weston Tollette
-/// Edit Purpose: Adding in functionality so that the bobber indicator wont show up if up if raycast hits nothing when it was previously down.
-///  Also adding an event cordinator to tell fish within the radius when the bobber is gone.
+/// Edited:
+/// Edited By:
+/// Edit Purpose:
 /// 
 public class TestFishLure : MonoBehaviour
 {
@@ -24,10 +23,7 @@ public class TestFishLure : MonoBehaviour
     private GameObject castSpotPrefab; 
 
     [SerializeField] [Tooltip("Layer for Water")]
-    private LayerMask waterMask; // used to dictate what layer the bobber can be on
-
-    [SerializeField] [Tooltip("Layer for Fish")]
-    private LayerMask fishMask; // used for activating event of fish.
+    private LayerMask waterMask;
 
     [Header("Rod Variables")]
     [SerializeField] [Tooltip("Max Cast Distance")]
@@ -41,17 +37,14 @@ public class TestFishLure : MonoBehaviour
     
 
     private bool holdToCast = false; // This bool will tell if the player is holding down the cast trigger, used to see if the cast has occured or not yet.
-    private bool getInRange = false; // checks for if the indicator is in range, if not the bobber wont be placed when left click is let go.
+
     private float currentCastValue; // the value of the cast at a certain point
-    private float lureRadius; // how large the lure radius is
-    
 
     private void Start()
     {
         input.InteractEvent += HandleInteract;
         input.InteractCancelledEvent += HandleInteractCancelled;
         castSpotPrefab.SetActive(false); // Set inactive at the start cause why not
-        lureRadius = lurePrefab.GetComponent<SphereCollider>().radius; // insures the lure radius is the same as the radius that lure trigger is.
     }
 
     void Update()
@@ -73,7 +66,6 @@ public class TestFishLure : MonoBehaviour
             if(!castSpotPrefab.activeSelf)
             {// if lure is not already activated
                 castSpotPrefab.SetActive(true); // activate the lure.
-                getInRange=true;
             }
             
             float numInRange = Mathf.PingPong(Time.time * pongSpeed, castVary); // return a number between 0 and cast range
@@ -88,61 +80,32 @@ public class TestFishLure : MonoBehaviour
             // Originating from where raycast was hit, in the direction that is forward to the player, along the line.
             //Debug.Log(hit.point);
         }
-        else if(castSpotPrefab.activeSelf)
-        {
-            castSpotPrefab.SetActive(false);
-            getInRange=false;
-        }
     } // Need to at some point force this so that the bobber indicator cant exist within a certain range of the player. so its not too close to the boat.
 
 
     private void CastLure() 
     {
-        if (getInRange)
-        { 
-            lurePrefab.SetActive(true); 
-            lurePrefab.transform.position = castSpotPrefab.transform.position; 
-            // set the lure position to the position that the indicator used to be.
-        }
+        lurePrefab.SetActive(true); 
+        lurePrefab.transform.position = castSpotPrefab.transform.position; 
+        // set the lure position to the position that the indicator used to be.
     }
 
-    private void RetrieveLure(Vector3 currentLurePosition) // this will let the fish know they are no longer in lure zone
-    {   
-        Vector3 lurePos = currentLurePosition; 
-        // Get all colliders within the sphere radius at this object's position
-        Collider[] hitColliders = Physics.OverlapSphere(lurePos, lureRadius, fishMask);
-        // might want to make this non alloc version.
-        
-        int i = 0;
-        
-        while (i < hitColliders.Length)
-        {
-            // Try to get the TargetScript component from the hit object
-            FishStateController target = hitColliders[i].GetComponent<FishStateController>();
-            
-            if (target != null)
-            {
-                target.LureReeledIn(); 
-                //Debug.Log("Lure Retrived");
-            }
-            i++;
-        }
-
-    }
     
     private void HandleInteract() // When you click the left mouse button
     {
         if (lurePrefab.activeSelf)
         {// if there is a lure already out only get rid of it, dont start the cast process again.
-            Vector3 currentLurePosition = lurePrefab.transform.position;
-            // will be used to get the lures position right before it is deactivated.
-
             lurePrefab.SetActive(false);
-            RetrieveLure(currentLurePosition); // this will let the fish know they are no longer in lure zone
-            
             return;
         }
         holdToCast = true;
+
+        if(castSpotPrefab.activeSelf)
+        { // This will asume the lure has no childern.
+            castSpotPrefab.SetActive(false); // disable Lure
+            return; // End here cause we dont need to do the rest of the script.
+        }// possibly look into Object pooling, might not be usefull now since i decided against just destroying and remaking it every time.
+        castSpotPrefab.SetActive(true);
     }
     private void HandleInteractCancelled() // when release left mouse
     {
@@ -151,7 +114,6 @@ public class TestFishLure : MonoBehaviour
             holdToCast = false;
             castSpotPrefab.SetActive(false);
             CastLure();
-            getInRange = false; // since lure is cast the the get in range is no longer necisarry.
         }
     }
 }
