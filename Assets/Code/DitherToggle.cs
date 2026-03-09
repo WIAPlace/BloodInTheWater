bc2b927f046509f2c148f4a4df1c3590c8abe99f
MDT <bonkwizardturner@gmail.com>
2026-03-08 22:30:55 -0500

    Pause Menu Stuff
    Dither Toggle
    How To Play Panel

---
+++ Assets/Code/DitherToggle.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+/// 
+/// Author: Marsahll Turner
+/// Created: 3/8/26
+/// Purpose: To toggle on/off the dither effect on screen
+/// 
+/// Edited: 
+/// Edited By: 
+/// Edit Purpose: 
+/// 
+public class DitherToggle : MonoBehaviour
+{
+    public bool ditherToggle;
+    public RenderTexture ditherTexture;
+    public GameObject ditherImage;
+
+    public void DitherSwitch()
+    {
+        ditherToggle = !ditherToggle;
+        
+        if (!ditherToggle)
+        {
+            ditherImage.SetActive(false);
+            Camera.main.targetTexture = null;
+        }
+        else
+        {
+            ditherImage.SetActive(true);
+            Camera.main.targetTexture = ditherTexture;
+        }
+    }
+}
