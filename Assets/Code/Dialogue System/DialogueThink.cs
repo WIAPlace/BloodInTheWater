72081ecfb75ee72a5cf01d4913a091cff774665d
MDT <bonkwizardturner@gmail.com>
2026-03-11 00:06:21 -0500

    Press “T” to Think

---
+++ Assets/Code/Dialogue System/DialogueThink.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// 
+/// Author: Marsahll Turner
+/// Created: 2/12/26
+/// Purpose: Sets the Object to Have Dialogue
+/// 
+/// Edited: 2/21/26
+/// Edited By: Weston Tollette
+/// Edit Purpose: implemented the Interactable Interface so that it is usable with the new interact system.
+/// 
+public class DialogueThink : MonoBehaviour
+{
+    [SerializeField] bool firstInteraction = true;
+    [SerializeField] bool noRepeat = true;
+    [SerializeField] int repeatStartPosition;
+
+    public string npcName;
+    public DialogueAsset dialogueAsset;
+    public InteractSystem interactSystem;
+
+    [HideInInspector]
+    public int StartPosition
+    {
+        get
+        {
+            if (firstInteraction)
+            {
+                firstInteraction = false;
+                if (noRepeat)
+                {
+                    gameObject.layer = LayerMask.NameToLayer("Default"); //Changes the layer to hide the crosshair hover
+                }
+                return 0;
+            }
+            else
+            {
+                return repeatStartPosition; //Starts the dialogue at the set line. Set it to a line that doesn't exist to disable the ability to talk again
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!interactSystem.inConversation)
+            {
+                Interact();
+            }
+            Debug.Log("Busy");
+        }
+    }
+
+    public void Interact() // Implemented from interface to start the conversation.
+    {
+        DialogueBoxController.instance.StartDialogue(dialogueAsset.dialogue,dialogueAsset.audioclip, StartPosition, npcName);
+    }
+}
