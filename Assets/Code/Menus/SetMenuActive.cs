a58d518a3c357914e8e50cc2ba57dbe771851d08
WesInAPlace <143664522+WIAPlace@users.noreply.github.com>
2026-03-23 17:13:31 -0500

    Added Cameras To Main Scene

--- Assets/Code/Menus/SetMenuActive.cs
+++ Assets/Code/Menus/SetMenuActive.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEditor;
 using UnityEngine;
 /// 
@@ -11,19 +12,29 @@ public class SetMenuActive : MonoBehaviour
 {
     [SerializeField][Tooltip("Array of Menus")] 
     GameObject[] menu;
+    [SerializeField][Tooltip("Array of Cameras")]
+    CinemachineCamera[] cameras; // array of cameras
 
     public void MenuActivate(int option)
     { // will run through all the menus and make sure they are inactive
         if(option<menu.Length && option>=0){ // make sure this only runs if the int is within the range.
-            foreach(GameObject obj in menu)
+            for(int i = 0; i<menu.Length;i++)
             {
-                if (obj.activeSelf)
+                if (menu[i].activeSelf)
                 { // only make inactive if it is already active.
-                    obj.SetActive(false);
+                    menu[i].SetActive(false);
+                    if (cameras[i] != null)
+                    {
+                        cameras[i].Priority =0;
+                    }
                 }
             }
             
             menu[option].SetActive(true); // set chosen menu to active.
+            if (cameras[option] != null)
+            { // set priority camea
+                cameras[option].Priority = 3;
+            }
         }
     }
 }
