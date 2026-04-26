cfe3367d1f588df55cf8e28f1a926e92ed8c1a59
WesInAPlace <143664522+WIAPlace@users.noreply.github.com>
2026-04-26 13:12:34 -0500

    FogWIP

--- Assets/Code/Time/TimeKeeper.cs
+++ Assets/Code/Time/TimeKeeper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -16,12 +17,22 @@ public class TimeKeeper : MonoBehaviour
     [SerializeField] private bool limitTime = true;
     [SerializeField] [Tooltip("Allocated Time To Fish")]
     float secondsAllocated;
-
+    
     [SerializeField] [Tooltip("Time Between Each checkign of the timer")]
     float waitInterval=5f; // we are doing it like this because we dont need to have this update every frame.
+
+    [SerializeField,Tooltip("How long it will take for the fog to roll in")]
+    private float fogTime;
+    [SerializeField]
+    float fogSmoothTime;
+    float fogVelocity;
+
+    [SerializeField] GameObject fogObject;
+
     [SerializeField] FlickerLights flickerLights;
     [SerializeField,Tooltip("lights will start flickering at: secondsAllocated - secondsAllocated/flickerPercent")]
     private float flickerPercent = 0;
+    
 
     private float sceneTime = 0;
     private float timePassed=0;
@@ -31,6 +42,9 @@ public class TimeKeeper : MonoBehaviour
     void Start()
     {
         //GameState.Instance.ChangeTime(GetTimeLeft());
+        if(fogObject!=null){ // we dont need this in scenes that dont limit time.
+            fogObject.SetActive(false);
+        }
         if (limitTime)
         {
             StartCoroutine(TickTock());
@@ -64,7 +78,7 @@ public class TimeKeeper : MonoBehaviour
             //GameState.Instance.ChangeTime(GetTimeLeft());
             //Debug.Log(timePassed);
         }
-       GameState.Instance.LooseState();
+       StartCoroutine(FogRollsIn());
     }
 
     public void AddPenaltyTime(float seconds)
@@ -104,5 +118,33 @@ public class TimeKeeper : MonoBehaviour
     public float GetCurrentTime()
     {
         return secondsAllocated-timePassed;
+    }
+
+    IEnumerator FogRollsIn()
+    {
+        fogObject.SetActive(true);
+        sceneTime = 0;
+        penaltyTime = 0;
+        while(timePassed < fogTime)
+        {
+            yield return new WaitForSeconds(0.01f);
+            timePassed = sceneTime + penaltyTime;
+
+            float t = timePassed/fogTime; // percentage of the way there.
+            float lerpScale = Mathf.Lerp(1,.12f,t);
+            // find the desired change of magnitude 
+
+            float curMag = fogObject.transform.localScale.x;
+            //Debug.Log(curMag);
+            // gett current magnitude
+
+            float newMag = Mathf.SmoothDamp(curMag,lerpScale,ref fogVelocity,fogSmoothTime,fogSmoothTime);
+            // smooth damp between the two for a smooth tranistion even if boat is hurt.
+
+            fogObject.transform.localScale = Vector3.one*newMag;
+            // move the fog inwards as dictated by the new magnitude.
+        }
+        yield return new WaitForSeconds(10f);
+        GameState.Instance.LooseState();
     }
 }
